f4a129aaa8cc9436de3dd2346447bf981a2a4638
Jeff Garzik <jeff@garzik.org>
2009-03-27 23:46:14 -0400

    Trim trailing whitespace.
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- server/config.c
+++ server/config.c
@@ -161,7 +161,7 @@ static void cfg_elm_end (GMarkupParseContext *context,
 
 static void cfg_elm_text (GMarkupParseContext *context,
 			  const gchar	*text,
-			  gsize		text_len,  
+			  gsize		text_len,
 			  gpointer	user_data,
 			  GError	**error)
 {
